e3d05337b9de52158d2547d944cb843a0086dc7b
Édgar Salguero Dorokhin <9egrgti@9egrgti.LAPTOP>
2017-01-06 23:15:15 -0500

    Correcciones en las vistas y en el controlador

--- public/javascripts/appweather.js
+++ public/javascripts/appweather.js
@@ -14,13 +14,33 @@ angular.module('appweather', ['ui.router'])
         $urlRouterProvider.otherwise('cuadro');
     })
     .factory('common', function($http) {
-        common.getAll = function($ciudad){
-            return url($ciudad);
+        var common = {};
+        common.clima = {};
+        common.get = function($ciudad){
+            /*
+            La funcion urlweather($city) está difinida el en archivo urlweather.js y devuelve un string con
+            la URL del API del tiempo (la variable $appid se obtiene al registrarse en el sitio). Ejemplo:
+                "http://api.openweathermap.org/data/2.5/weather?q=" + $city + "&appid=" + $appid + "&lang=es"
+            */
+            var mydata = get_json(urlweather($ciudad), function(data) {
+                if (data.cod == "200") {
+                    angular.copy(data, common.clima);
+                    common.clima.main.temp = parseFloat(common.clima.main.temp) - 273.15;
+                    common.clima.dt = timeConverter(parseInt(common.clima.dt));
+                    return common.clima;
+                } else {
+                    alert("ERROR " + data.cod + ".\n" + data.message);
+                }
+            })
         }
+        return common;
     })
     .controller('ctrlCuadro', function($scope, $state, common) {
+        $scope.climas = {};
         $scope.comenzar = function() {
-            alert(common.getAll('Sangolqui'));
+            $scope.climas = new Array();
+            common.get($scope.ciudad);
+            $scope.climas = [common.clima];
         }
     })
     .controller('ctrlHistorico', function($scope, $state, common) {
